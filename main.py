ea416e5966bbd7487c0589cc5173c512330b6dad
kmracoma <kristinmay.racoma@infor.com>
2021-09-28 12:30:54 +0800

    Updated checking of new users

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ name = str(input("Please enter your name:"))
 
 for x in f:
     if name in x:
-        borrowed = x.split(string1,1)[1]
-        print(borrowed.strip())
-        if (borrowed.strip() <= '3'):
+        borrowed = x.split(string1,1)[1].strip()
+        print(borrowed)
+        if (borrowed < '3'):
             print("Allowed to borrow")
             break
         else:
             print("Borrowed number of books exceeded")
             break
-    else:
-        print("New user found!")
-        f.write("\nName: " + name + " Books: 0")
+else:
+    print("New user found!")
+    f.write("\nName: " + name + " Books: 0")
+    print("Record updated!")
 
 f.close()
